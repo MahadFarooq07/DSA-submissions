f869db79621a2ef90c2136225ed13e4a029866e3
Mahad Farooq <mahadfarooq133@gmail.com>
2026-09-12 14:08:55 -0400

    Add: longest-repeating-substring-with-replacement - submission-11

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-11.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+       let left = 0
+       let map = new Map()
+       let max = 0
+
+
+       for(let right = 0; right<s.length;right++){
+        
+        map.set(s[right], ((map.get(s[right])) || 0)+1)
+        
+        while((right - left + 1) - (Math.max(...map.values())) > k){
+            map.set(s[left],(map.get(s[left]))-1)
+            left++
+        }
+
+        max = Math.max(max, right- left + 1)
+
+       }
+       return max
+
+    }
+}
